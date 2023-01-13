74089981f48552b338cfbc2326af16063e702f23
keskesbilel1 <78603251+keskesbilel1@users.noreply.github.com>
2023-01-13 20:25:00 +0100

    modifier par bilel keskes

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,7 +17,7 @@ export default function Home() {
       <main className={styles.main}>
         <div className={styles.description}>
           <p>
-          Commencez par modifier&nbsp;
+           Modifier par bilel keskes &nbsp;
             <code className={styles.code}>pages/index.tsx</code>
           </p>
           <div>
