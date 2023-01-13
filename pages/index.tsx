99902e88de8065cc9654bd62e0a51e1115648b3f
keskesbilel1 <78603251+keskesbilel1@users.noreply.github.com>
2023-01-13 20:20:28 +0100

    Commencez par modifier

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,7 +17,7 @@ export default function Home() {
       <main className={styles.main}>
         <div className={styles.description}>
           <p>
-          Commencez par modifier &nbsp;
+          Commencez par modifier&nbsp;
             <code className={styles.code}>pages/index.tsx</code>
           </p>
           <div>
